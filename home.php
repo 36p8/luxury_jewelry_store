695cc6ca4105ac34832279d2b8c6fa9dc82c81bc
wala12-7 <wala85279@gmail.com>
2026-05-29 00:37:16 +0300

    Fix image paths and structure in home.php

--- home.php
+++ home.php
@@ -25,19 +25,20 @@
         <h3>تصنيفات مختارة</h3>
     </div>
     <div class="categories-grid">
-        <div class="category-card">
-            <div class="img-container">
-                <img src="images/ring.jpg" alt="Luxury Rings">
-            </div>
-            <h4>خواتم فاخرة</h4>
+    <div class="category-card">
+        <div class="img-container">
+            <img src="home1.jpg.jpg" alt="Luxury Rings">
         </div>
-        <div class="category-card">
-            <div class="img-container">
-                <img src="images/bracelet.jpg" alt="Luxury Bracelets">
-            </div>
-            <h4>أساور راقية</h4>
-        </div>
+        <h4>خواتم فاخرة</h4>
     </div>
+
+    <div class="category-card">
+        <div class="img-container">
+            <img src="home2.jpg.jpg" alt="Luxury Bracelets">
+        </div>
+        <h4>أساور راقية</h4>
+    </div>
+</div>
 </section>
 
 <?php include 'includes/footer.php'; ?>
